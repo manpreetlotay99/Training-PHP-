9514a23025bcd83292fc09933042e8cb5444a970
Manpreet Kaur <53974400+manpreetlotay99@users.noreply.github.com>
2023-02-23 14:07:39 +0530

    Update oopscalc.php

--- oopscalc.php
+++ oopscalc.php
@@ -1,5 +1,8 @@
 <html>
 <title> Form</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <body>
 <?php
 class calculator{
@@ -26,11 +29,16 @@ class calculator{
 	}
 	
 }
-$calc= new calculator(23,2);
-echo $calc-> add()."\n"; 
-echo $calc-> sub()."\n"; 
-echo $calc-> multiply()."\n"; 
-echo $calc-> divide()."\n"; 
 ?>
+<div class= "container">
+<h3> CALCULATOR </h3>
+<?php
+$calc= new calculator(23,2);
+echo "Result after Addition: ". $calc-> add()."\n"; 
+echo "Result after Subtraction: ". $calc-> sub()."\n"; 
+echo "Result after Multiplication: ". $calc-> multiply()."\n"; 
+echo "Result after division: ". $calc-> divide()."\n"; 
+?>
+</div>
 </body>
 </html>
